38b967e29b7a795be11535a38853511ade687ba6
Robert Usey <ruusey@gmail.com>
2024-02-04 11:14:15 -0500

    Update PlayerDataService.java

--- src/main/java/com/jrealm/data/service/PlayerDataService.java
+++ src/main/java/com/jrealm/data/service/PlayerDataService.java
@@ -1,9 +1,9 @@
 package com.jrealm.data.service;
 
-import java.util.UUID;
 import java.time.Instant;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,16 +70,15 @@ public class PlayerDataService {
 				}
 			}
 		} catch (Exception e) {
-			log.error("Failed to seed Player Account. Reason: {}", e);
+			PlayerDataService.log.error("Failed to seed Player Account. Reason: {}", e);
 		}
 	}
-	
+
 	public CharacterDto saveCharacterStats(final String characterUuid, final CharacterDto newData) throws Exception{
 		CharacterEntity character = this.playerCharacterRepository.findByCharacterUuid(characterUuid);
-		if(character==null) {
+		if(character==null)
 			throw new Exception("Character with UUID "+characterUuid+" was not found.");
-		}
-		
+
 		character.getStats().setHp(newData.getStats().getHp());
 		character.getStats().setMp(newData.getStats().getMp());
 		character.getStats().setDef(newData.getStats().getDef());
@@ -89,7 +88,7 @@ public class PlayerDataService {
 		character.getStats().setWis(newData.getStats().getWis());
 		character.getStats().setVit(newData.getStats().getVit());
 		character.getStats().setXp(newData.getStats().getXp());
-		
+
 		for(GameItemRefEntity gameItem : character.getItems()) {
 			//character.removeItem(gameItem);
 			this.deleteGameItem(gameItem);
@@ -98,16 +97,18 @@ public class PlayerDataService {
 		character = this.playerCharacterRepository.save(character);
 
 		for(GameItemRefDto item: newData.getItems()) {
-			if(item==null) continue;
+			if(item==null) {
+				continue;
+			}
 			GameItemRefEntity itemEntity = this.mapper.map(item, GameItemRefEntity.class);
 			itemEntity = this.gameItemRefRepository.save(itemEntity);
 			character.addItem(itemEntity);
-		}		
+		}
 
 		character = this.playerCharacterRepository.save(character);
 		return this.mapper.map(character, CharacterDto.class);
 	}
-	
+
 	public PlayerAccountDto saveAccount(final PlayerAccountDto dto) {
 		PlayerAccountEntity entity = this.mapper.map(dto, PlayerAccountEntity.class);
 		entity = this.playerAccountRepository.save(entity);
@@ -120,18 +121,18 @@ public class PlayerDataService {
 		// Build a new account with a random uuid and the provided email + accountName;
 		final PlayerAccountEntity account = PlayerAccountEntity.builder().accountEmail(email).accountName(accountName)
 				.accountUuid(accountUuid).build();
-		
+
 		// Create a single chest with one item in it
 		final ChestEntity initialChest = ChestEntity.builder().chestUuid(PlayerDataService.randomUuid()).ordinal(0).build();
 		// Create a new GameItemRef and put it in this chest
-		
+
 		final GameItemRefEntity gameItemDBow = GameItemRefEntity.from(0, 47);
 		// Add the item to the chest
 		initialChest.addItem(gameItemDBow);
-		
+
 		// Build a character from the provided classId, give it a weapon and give it default stats from GameDataManager
 		final CharacterEntity character = CharacterEntity.builder().characterUuid(PlayerDataService.randomUuid()).characterClass(characterClass).build();
-		
+
 		// Equip the player with their starting equipment
 		final Map<Integer, GameItem> startingEquip = GameDataManager.getStartingEquipment(CharacterClass.valueOf(characterClass));
 		for(int i = 0; i<startingEquip.values().size(); i++) {
@@ -139,44 +140,40 @@ public class PlayerDataService {
 			final GameItemRefEntity toEquipEntity = GameItemRefEntity.from(i, toEquip.getItemId());
 			character.addItem(toEquipEntity);
 		}
-		
+
 		final CharacterStatsEntity characterStats = CharacterStatsEntity.characterDefaults(characterClass);
 		character.setStats(characterStats);
 
 		account.addCharacter(character);
 		account.addChest(initialChest);
-		
+
 		final PlayerAccountEntity finalAccount = this.playerAccountRepository.save(account);
-		
+
 		//this.replaceChestItem(initialChest.getChestUuid(), gameItemDBow.getItemUuid(), null);
-		log.info("Successfully created account for user {} in {}ms", finalAccount.getAccountEmail(), (Instant.now().toEpochMilli()-start));
+		PlayerDataService.log.info("Successfully created account for user {} in {}ms", finalAccount.getAccountEmail(), (Instant.now().toEpochMilli()-start));
 		return this.mapper.map(finalAccount, PlayerAccountDto.class);
 	}
-	
+
 	public boolean replaceChestItem(final String chestUuid, final String targetItemUuid, final GameItemRefEntity replacement) throws Exception{
 		final ChestEntity targetChest = this.playerChestRepository.findByChestUuid(chestUuid);
 		boolean success = false;
-		if(targetChest==null) {
+		if(targetChest==null)
 			throw new Exception("Chest with UUID "+chestUuid+" does not exist");
-		}
 		final PlayerAccountEntity ownerAccount = targetChest.getOwnerAccount();
 		final GameItemRefEntity targetItem = this.gameItemRefRepository.findByItemUuid(targetItemUuid);
-		if(targetItem==null) {
+		if(targetItem==null)
 			throw new Exception("Target item with UUID "+targetItemUuid+ " does not exist");
-		}
 		if(replacement==null) {
 			Optional<GameItemRefEntity> itemInChest = targetChest.getItems().stream().filter(item->item.getItemUuid().equals(targetItemUuid)).findAny();
-			if(itemInChest.isEmpty()) {
+			if(itemInChest.isEmpty())
 				throw new Exception("Target item with UUID "+targetItemUuid+ " does not exist in chest with UUID "+chestUuid );
-			}
 			final GameItemRefEntity toRemove = itemInChest.get();
 			success = targetChest.removeItem(targetItem);
 			this.deleteGameItem(toRemove);
 		}else {
 			Optional<GameItemRefEntity> itemInChest = targetChest.getItems().stream().filter(item->item.getItemUuid().equals(targetItemUuid)).findAny();
-			if(itemInChest.isEmpty()) {
+			if(itemInChest.isEmpty())
 				throw new Exception("Target item with UUID "+targetItemUuid+ " does not exist in chest with UUID "+chestUuid );
-			}
 			final GameItemRefEntity toRemove = itemInChest.get();
 			success = targetChest.removeItem(targetItem);
 			this.deleteGameItem(toRemove);
@@ -185,71 +182,64 @@ public class PlayerDataService {
 		this.playerAccountRepository.save(ownerAccount);
 		return success;
 	}
-	
+
 	public void deleteGameItem(final GameItemRefEntity toDelete) {
 		this.gameItemRefRepository.delete(toDelete);
 
 	}
-	
+
 	public void hardDeleteGameItem(final GameItemRefEntity toDelete) throws Exception{
 		this.gameItemRefRepository.delete(toDelete.getItemUuid());
 	}
-	
+
 	public void hardDeleteGameItem(final String gameItemUuid) throws Exception {
 		final GameItemRefEntity toDelete = this.gameItemRefRepository.findByItemUuid(gameItemUuid);
-		if(toDelete==null) {
+		if(toDelete==null)
 			throw new Exception("GameItem with UUID "+ gameItemUuid+ " does not exist");
-		}
 		this.gameItemRefRepository.delete(toDelete.getItemUuid());
 	}
-	
+
 	public PlayerAccountDto getAccountById(final Integer accountId) throws Exception {
 		Optional<PlayerAccountEntity> entity = this.playerAccountRepository.findById(accountId);
-		if(entity.isPresent()) {
-			return mapper.map(entity.get(), PlayerAccountDto.class);
-		}else{
-			throw new Exception("PlayerAccount with id "+ accountId+" not found");
-		}
+		if(entity.isPresent())
+			return this.mapper.map(entity.get(), PlayerAccountDto.class);
+		throw new Exception("PlayerAccount with id "+ accountId+" not found");
 	}
-	
+
 	public PlayerAccountDto getAccountByEmail(final String email) throws Exception {
 		Optional<PlayerAccountEntity> entity = this.playerAccountRepository.findByAccountEmail(email);
-		if(entity.isPresent()) {
-			return mapper.map(entity.get(), PlayerAccountDto.class);
-		}else{
-			throw new Exception("PlayerAccount with email "+ email+" not found");
-		}
+		if(entity.isPresent())
+			return this.mapper.map(entity.get(), PlayerAccountDto.class);
+		throw new Exception("PlayerAccount with email "+ email+" not found");
 	}
-	
+
 	public PlayerAccountDto getAccountByUuid(final String accountUuid) throws Exception {
 		Optional<PlayerAccountEntity> entity = this.playerAccountRepository.findByAccountUuid(accountUuid);
-		if(entity.isPresent()) {
-			return mapper.map(entity.get(), PlayerAccountDto.class);
-		}else{
-			throw new Exception("PlayerAccount with account UUID "+ accountUuid+" not found");
-		}
+		if(entity.isPresent())
+			return this.mapper.map(entity.get(), PlayerAccountDto.class);
+		throw new Exception("PlayerAccount with account UUID "+ accountUuid+" not found");
 	}
-	
-//	private GameItemRefEntity newGameItem(final int gameItemId) throws Exception{
-//		GameItem model = GameDataManager.GAME_ITEMS.get(gameItemId);
-//		if(model == null) {
-//			throw new IllegalArgumentException("GameItem with id "+gameItemId+" does not exist.");
-//		}
-//		return GameItemRefEntity.builder().itemId(49).itemUuid(randomUuid()).build();
-//	}
-//		
-//	private ChestEntity newChest(final int ordinal) {
-//		return ChestEntity.builder().ordinal(ordinal).build();
-//	}
-//	
-//	private CharacterEntity newCharacter(final int characterClassId) {
-//		return CharacterEntity.builder().characterClass(characterClassId).build();
-//	}
-//	
-//	private PlayerAccountEntity newPlayerAccount(final String email, final String accountName) {
-//		return PlayerAccountEntity.builder().accountEmail(email).accountName(accountName)
-//				.accountUuid(PlayerDataService.randomUuid()).build();
-//	}
+
+	//	private GameItemRefEntity newGameItem(final int gameItemId) throws Exception{
+	//		GameItem model = GameDataManager.GAME_ITEMS.get(gameItemId);
+	//		if(model == null) {
+	//			throw new IllegalArgumentException("GameItem with id "+gameItemId+" does not exist.");
+	//		}
+	//		return GameItemRefEntity.builder().itemId(49).itemUuid(randomUuid()).build();
+	//	}
+	//
+	//	private ChestEntity newChest(final int ordinal) {
+	//		return ChestEntity.builder().ordinal(ordinal).build();
+	//	}
+	//
+	//	private CharacterEntity newCharacter(final int characterClassId) {
+	//		return CharacterEntity.builder().characterClass(characterClassId).build();
+	//	}
+	//
+	//	private PlayerAccountEntity newPlayerAccount(final String email, final String accountName) {
+	//		return PlayerAccountEntity.builder().accountEmail(email).accountName(accountName)
+	//				.accountUuid(PlayerDataService.randomUuid()).build();
+	//	}
 
 	public static String randomUuid() {
 		return UUID.randomUUID().toString();
