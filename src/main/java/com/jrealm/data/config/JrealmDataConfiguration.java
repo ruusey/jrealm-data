51d4ed0c93ff3214c889e5369b64c2d70fbbdaf3
Robert Usey <ruusey@gmail.com>
2024-08-30 13:19:41 -0400

    Small tweaks and version bump

--- src/main/java/com/jrealm/data/config/JrealmDataConfiguration.java
+++ src/main/java/com/jrealm/data/config/JrealmDataConfiguration.java
@@ -1,7 +1,6 @@
 package com.jrealm.data.config;
 
 import org.modelmapper.ModelMapper;
-import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
@@ -10,8 +9,6 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-
-import io.swagger.v3.oas.models.parameters.Parameter;
 
 @Configuration
 public class JrealmDataConfiguration extends AbstractMongoClientConfiguration {
@@ -28,26 +25,12 @@ public class JrealmDataConfiguration extends AbstractMongoClientConfiguration {
 	@Override
 	@Bean
 	public MongoClient mongoClient() {
-		//final ConnectionString connectionString = new ConnectionString("mongodb://host.docker.internal:27017/jrealm");
-		final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/jrealm");
+	        final ConnectionString connectionString = new ConnectionString("mongodb://127.0.0.1:27017/jrealm");
+		//final ConnectionString connectionString = new ConnectionString("mongodb://18.117.127.158:27017/jrealm");
 		final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
 				.applyConnectionString(connectionString).build();
 		return MongoClients.create(mongoClientSettings);
 	}
 
 	
-	@Bean
-	public OperationCustomizer customize() {
-		return (operation, handlerMethod) -> {
-			if(!handlerMethod.getMethod().getName().contains("login") && !handlerMethod.getMethod().getName().contains("ping")) {
-				operation.addParametersItem(
-				new Parameter()
-                .in("header")
-                .required(true)
-                .description("JRealm Authorization Token")
-                .name("Authorization"));
-			}
-			return operation;
-		};
-	}
 }
